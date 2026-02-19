1628048cf0ce4ba6f6f08f8e9a3ed8d83403a8cf
k8bus <K8bus@proton.me>
2026-02-19 21:37:55 +0100

    navbar got own life and get allowance(is in separated files :))

---
+++ src/data/navigation.ts
@@ -0,0 +1,18 @@
+export const navConfig = {
+    links: [
+        { name: 'MODES', href: '/modes' },
+        { name: 'HOME', href: '/' },
+        { name: 'INSIDES', href: '/blog' },
+    ],
+    shop: {
+        name: 'SHOP',
+        href: '/shop',
+    },
+    server: {
+        ip: 'PLAY.TURBOBLOCKS.EU',
+        discord: 'https://discord.gg/vuG3EQtV4',
+    },
+    cart: {
+        href: '/cart',
+    }
+};
